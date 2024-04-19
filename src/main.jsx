b39a7cc64a86f96964da010d521875ccc5f6e6d0
Jeffrey Hu <zhiwehu@gmail.com>
2024-04-19 17:35:17 +0800

    init

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-1dgia33wzw8geq5g.us.auth0.com"
+      clientId="B5AA8tnO3S497yKuZP6KLzyIXVYqB3D3"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
